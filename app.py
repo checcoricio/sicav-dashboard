46db90a7c42e26c47e393a1ece367ca9f3435000
checcoricio <checcoricio@gmail.com>
2026-06-19 15:25:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ CHART_PALETTE = [
 ]
 
 # Date di default ragionevoli (modificabili dall'utente)
-DEFAULT_START = datetime.date(2026, 1, 1)
+DEFAULT_START = datetime.date(2025, 12, 29)
 DEFAULT_END = datetime.date.today()
 
 
